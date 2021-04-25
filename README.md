99ae59e74bb07858941392637e87f2196a49ab82
Scott Malone <scottmcarpentry@gmail.com>
2021-04-25 21:00:31 +1000

    Completed Requirements

---
+++ README.md
@@ -0,0 +1,9 @@
+#### Hello Reelit Team.
+
+#### How I first designed how the system would work:
+
+I have built a few websites now that utilise json and buttons. For this task I did not really plan the task. I instantly new that I can import a button from bootstrap and use CSS flexbox to justify and align the button into the middle of the body of the webpage as long as I set the body to viewport height and viewport width.  Sending an array via a python list is something I am very comfortable with aswell. So I mapped it out in my head. I knew if I get stuck that I have the code available for my old website that I can refer to. On bigger tasks I may write pseudo code. I have also completed wireframes and ERD's for bigger websites.
+
+#### What sources I used to repurpose:
+
+I have built myself a website template that is built in an MVC pattern that I have stored on my personal computer. I am refining the website template more and more every project. So you could call that a repurpose website template. You can check the source code at https://github.com/ScottBurleighHeads/Website_template. Also I rely on bootstrap with a bit of CSS for designing user interfaces. I am sure you guys are aware of bootstrap already but if not it is a source for pre-built CSS and Javascript.
--- src/controllers/index.py
+++ src/controllers/index.py
@@ -1,6 +1,37 @@
 from flask import Blueprint, render_template
+import requests, json
 index = Blueprint("index",__name__)
 
-@index.route("/hello")
-def hello_world():
+@index.route("/home",methods=["GET"])
+def home():
     return render_template("index.html")
+
+@index.route("/getTemplates",methods=["GET","POST"])
+def get_templates():
+    response = requests.get(f"https://my-json-server.typicode.com/reelit/reelittakehome/templates")
+    list_response = json.loads(response.text)
+
+    genre_list = []
+    for item in list_response:
+        if item['template_genre']=='genre_1':
+            genre_list.append(item)
+
+    holder = list_response[0]
+    for num,item in enumerate(list_response):
+        if  item["total_length"] < holder["total_length"]:
+            temp = list_response[num]
+            list_response[num]=holder
+            holder=temp
+        else:
+            holder = list_response[num]
+    
+    list_response.reverse()
+    filterOrder = []
+    for item in list_response:
+        if item['template_genre']=='genre_1':
+            filterOrder.append(item)
+    
+    names = ["Filtered","Ordered","Filtered and Ordered"]
+    dictList=[genre_list,list_response,filterOrder]
+                
+    return render_template("data.html",dictList=dictList,names=names)
